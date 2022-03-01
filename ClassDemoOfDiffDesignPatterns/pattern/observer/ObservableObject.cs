4306c1e603fd4b01820ee392acf52ea288921637
Peter Levinsky <pele@easj.dk>
2022-03-01 11:08:36 +0100

    Add project files.

---
+++ ClassDemoOfDiffDesignPatterns/pattern/observer/ObservableObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ClassDemoOfDiffDesignPatterns.Annotations;
+
+namespace ClassDemoOfDiffDesignPatterns.pattern.observer
+{
+    class ObservableObject 
+    {
+        private int _id;
+        private String _text;
+
+        public ObservableObject(int id, string text)
+        {
+            _id = id;
+            _text = text;
+        }
+
+        public ObservableObject()
+        {
+        }
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value == _id) return;
+                _id = value;
+                
+            }
+        }
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (value == _text) return;
+                _text = value;
+                
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Text)}: {Text}";
+        }
+
+
+        
+    }
+}
---
+++ ClassDemoOfDiffDesignPatterns/pattern/singleton/NoteCatalogue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ClassDemoOfDiffDesignPatterns.pattern.singleton
+{
+    class NoteCatalogue
+    {
+        /*
+         * Singleton
+         */
+
+        // trin 1 - Private konstruktor
+        private NoteCatalogue()
+        {
+            this.notes = new List<string>()
+            {
+                "note1", "note2", "and note3"
+            };
+        }
+        
+        // trin 2 - instans felt
+        private static NoteCatalogue _instance = new NoteCatalogue();
+
+        // trin 3 - statisk get property - egentlig en factory metode
+        public static NoteCatalogue Instance
+        {
+            get { return _instance; }
+        }
+
+        /*
+         * Slut singleton
+         */
+
+
+        private List<String> notes;
+
+        public List<string> Notes => new List<string>(notes);
+
+
+
+        public void Add(string item)
+        {
+            notes.Add(item);
+        }
+
+        public void Clear()
+        {
+            notes.Clear();
+        }
+
+
+        public override string ToString()
+        {
+            String strNotes = String.Join("\n", notes);
+            return $"Notes are :\n{strNotes}";
+        }
+    }
+}
